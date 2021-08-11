5a437833bb2d966eec58cc5e6c1873a61ed94782
Radek Skladanowski <radek.skladanowski@yahoo.com>
2021-08-11 21:49:49 +0200

    Hackerrank Reflection Task

---
+++ Hackerrank_reflection/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        bufferedReader.close();
+
+
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int result = 0;
+                result += arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1) +
+                        arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+                resultList.add(result);
+            }
+        }
+
+        Collections.sort(resultList);
+        System.out.println(resultList.get(resultList.size()-1));
+
+    }
+}
---
+++ Hackerrank_reflection/src/com/company/Student.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Student {
+
+    private String name;
+    private String lastName;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void newMethod(){
+    }
+
+    public static void anotherMethod() {}
+
+    private void aaaMethod() {}
+}
